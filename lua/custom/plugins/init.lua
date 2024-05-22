b8a8bc1444b0bd4ca2c856b352661c63978383fb
Matthew <matthewg_@hotmail.com>
2024-05-21 23:11:32 -0500

    BATMAN

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,9 @@
+--   You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  { -- Add indentation guides even on blank lines
+    'habamax/vim-godot',
+  },
+}
