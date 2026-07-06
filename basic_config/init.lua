8fa4a042f65e6d485be69d2ffe6f39a35626bfdb
goots <mgreen72194@gmail.com>
2026-07-05 23:26:29 -0500

    update basic config to have telescope, lsp + mason + nvim cmp, and others -> needs tree-sitter still then complete enough I think

--- basic_config/init.lua
+++ basic_config/init.lua
@@ -1,6 +1,10 @@
-require('remap')
-require('custom')
+require('goots.remap')
+require('goots.custom')
 
+
+local function package(org, repo)
+	return 'https://github.com/' .. org .. '/' .. repo
+end
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
@@ -10,4 +14,106 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+
 -- vim.pack.add{'https://github.com/neovim/nvim-lspconfig'}
+vim.pack.add{
+	package('folke', 'which-key.nvim'),
+	package('numToStr', 'Comment.nvim'),
+}
+
+--Colour schemes
+vim.pack.add{
+	package('luisiacc', 'gruvbox-baby'),
+}
+
+vim.cmd.colorscheme 'gruvbox-baby'
+-- transparent background
+vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+-- You can configure highlights by doing something like
+-- vim.cmd.hi 'Comment gui=none'
+
+-- Telescope
+vim.pack.add{
+	package('nvim-lua', 'plenary.nvim'),
+    package('nvim-telescope', 'telescope-fzf-native.nvim'),
+    package('nvim-telescope', 'telescope-ui-select.nvim'),
+	package('nvim-telescope', 'telescope.nvim')
+}
+
+require('telescope').setup {
+	extensions = {
+		['ui-select'] = {
+			require('telescope.themes').get_dropdown(),
+		},
+	},
+}
+
+pcall(require('telescope').load_extension, 'fzf')
+pcall(require('telescope').load_extension, 'ui-select')
+
+local builtin = require 'telescope.builtin'
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+
+vim.keymap.set('n', '<leader>/', function()
+	builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+		winblend = 10,
+		previewer = false,
+	})
+end, { desc = '[/] Fuzzily search in current buffer' })
+
+vim.keymap.set('n', '<leader>s/', function()
+	builtin.live_grep {
+		grep_open_files = true,
+		prompt_title = 'Live Grep in Open Files',
+	}
+end, { desc = '[S]earch [/] in Open Files' })
+
+vim.keymap.set('n', '<leader>sn', function()
+	builtin.find_files { cwd = vim.fn.stdpath 'config' }
+end, { desc = '[S]earch [N]eovim files' })
+
+--LSP
+vim.pack.add{ 
+	package('hrsh7th', 'nvim-cmp'),
+	package('hrsh7th', 'cmp-nvim-lsp'),
+	package('hrsh7th', 'cmp-path'),
+	package('nvim-mini', 'mini.nvim'),
+	package('neovim', 'nvim-lspconfig'),
+	package('mason-org', 'mason.nvim'),
+}
+require('mason').setup()
+
+vim.diagnostic.config({ virtual_text = true })
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = vim.tbl_deep_extend("force", capabilities, require("mini.completion").get_lsp_capabilities())
+
+vim.lsp.config("*", { capabilities = capabilities })
+
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('clangd')
+vim.lsp.enable('ols')
+local cmp = require 'cmp'
+cmp.setup {
+	completion = { completeopt = 'menu,menuone,noinsert' },
+	mapping = cmp.mapping.preset.insert {
+		['<C-n>'] = cmp.mapping.select_next_item(),
+		['<C-p>'] = cmp.mapping.select_prev_item(),
+		['<Tab>'] = cmp.mapping.confirm { select = true },
+		['<C-Space>'] = cmp.mapping.complete {},
+	},
+
+	--Sets up compleion menu
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+		{ name = 'path' },
+	},
+}
+
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {desc = "Go to def"})
+
---
+++ basic_config/lua/utils.lua
@@ -0,0 +1,6 @@
+local M = {}
+
+M.package = package
+
+return M
+
